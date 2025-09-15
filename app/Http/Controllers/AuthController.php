ffbce7ca57f4445236d2d3d22716031028dbabfc
leuldlt <leuld08@gmail.com>
2025-09-15 15:46:46 +0300

    fix:password cjhange fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-//use App\Models\Modeltblusers;
+use App\Models\Modeltblusers;
 use Illuminate\Support\Facades\Validator;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\DB;
