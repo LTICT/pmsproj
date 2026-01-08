8c805d1717f593c9f3f6b32edd1eaabf72220d0e
leuldlt <leuld08@gmail.com>
2026-01-08 16:20:40 +0300

    fix:add amharic and english description

--- app/Http/Controllers/Api/PmsprojectController.php
+++ app/Http/Controllers/Api/PmsprojectController.php
@@ -211,12 +211,13 @@ SELECT * FROM project_hierarchy';
         $permissionData=$this->getPagePermission($request,9, "project_info");
         //dd($permissionData);
         //dump($permissionData);
-        $query='SELECT prj_measured_figure,prj_measurement_unit, prj_parent_id,prj_object_type_id, prs_color_code AS color_code,prs_id AS status_id, prs_status_name_en AS status_name,add_name_or, prj_name_en,prj_name_am,prj_department_id,prj_id,prj_name,prj_code, prj_project_status_id,prj_project_category_id,prj_total_estimate_budget,prj_total_actual_budget,
+        $query='SELECT 
+            prj_location_description_en, prj_location_description_am, prj_measured_figure,prj_measurement_unit, prj_parent_id,prj_object_type_id, prs_color_code AS color_code,prs_id AS status_id, prs_status_name_en AS status_name,add_name_or, prj_name_en,prj_name_am,prj_department_id,prj_id,prj_name,prj_code, prj_project_status_id,prj_project_category_id,prj_total_estimate_budget,prj_total_actual_budget,
         prj_geo_location,prj_sector_id,prj_location_region_id,prj_location_zone_id,prj_location_woreda_id,
         prj_location_description,prj_owner_region_id,prj_owner_zone_id,prj_owner_woreda_id,prj_owner_description,
         prj_start_date_gc,prj_start_date_plan_gc,prj_end_date_actual_et,prj_end_date_actual_gc,
         prj_end_date_plan_gc,prj_outcome,prj_remark
-        ,prj_owner_id,prj_urban_ben_number,prj_rural_ben_number,1 AS is_editable, 1 AS is_deletable,prj_program_id FROM pms_project ';
+        ,prj_owner_id,prj_urban_ben_number,prj_rural_ben_number,1 AS is_editable, 1 AS is_deletable,prj_program_id, FROM pms_project ';
         $query .=' LEFT JOIN pms_project_status ON pms_project_status.prs_id= pms_project.prj_project_status_id';
         $query .=' LEFT JOIN gen_address_structure ON gen_address_structure.add_id= pms_project.prj_owner_zone_id';
         $query .=' WHERE prj_owner_type =1';
@@ -372,7 +373,7 @@ SELECT * FROM project_hierarchy';
             'prj_location_woreda_id',
             'prj_location_description',
             'prj_location_description_en',
-            'prj_location_description_or',
+            'prj_location_description_am',
             'prj_owner_region_id',
             'prj_owner_zone_id',
             'prj_owner_woreda_id',
